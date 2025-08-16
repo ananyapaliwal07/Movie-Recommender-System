67683d031956fcf22575b4fb8e66270635a3184a
Ananya Paliwal <ananya.paliwal2508@gmail.com>
2025-08-16 15:44:38 +0530

    updated app.py with gdown

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 import gdown
 import pickle
 import os
+os.system("pip install gdown")
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
